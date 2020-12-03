c1001aa9956761b593addeb93bc4e00604573bf6
ssukim <tntntn0930@gmail.com>
2020-12-03 13:26:50 +0900

    sendMail Function with Nodemailer

--- src/api/User/requestSecret/requestScret.js
+++ src/api/User/requestSecret/requestScret.js
@@ -7,6 +7,7 @@ export default {
       const { email } = args;
       const loginSecret = generateSecret();
       try{
+          await sendSecretMail(email, loginSecret);
           await prisma.updateUser({ data: { loginSecret }, where: { email } });
           return true;
       }catch(error) {
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,43 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { adjectives, nouns } from "./word";
+import nodemailer from "nodemailer";
+import mgTransport from "nodemailer-mailgun-transport";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+console.log(process.env.MAILGUN_API, process.env.MAILGUN_DOMAIN);
+export const sendMail = (email) => {
+  const options = {
+    auth: {
+      api_key: process.env.MAILGUN_API,
+      domain: process.env.MAILGUN_DOMAIN,
+    },
+  };
+  const client = nodemailer.createTransport(mgTransport(options));
+  return client
+    .sendMail(email)
+    .then(() => {
+      console.log("Message sent!");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+// 메일에 대한 내용을 다룹니다. sendMail을 통해 메일을 보냅니다.
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: "test@edupopkorn.com",
+    to: address,
+    subject: "Login Secret for Prismagram 🚀",
+    html: `<h1>hello! your login secret is ${secret}.</h1>
+    <h2>Copy paste on the web/app to Login</h2>`,
+  };
+  return sendMail(email);
 };
