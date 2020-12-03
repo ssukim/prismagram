6eebe3df8c0d1d4048775cda932d2aa99b15b775
ssukim <tntntn0930@gmail.com>
2020-12-03 10:45:41 +0900

    requestSecret Resolver

---
+++ src/utils.js
@@ -0,0 +1,6 @@
+import { adjectives, nouns } from "./word";
+
+export const generateSecret = () => {
+  const randomNumber = Math.floor(Math.random() * adjectives.length);
+  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
