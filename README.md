4326c66a8f0c9843c50dff0d1994cd6c59528103
ssukim <tntntn0930@gmail.com>
2020-12-21 13:31:10 +0900

    follow unfollow Resolver

--- README.md
+++ README.md
@@ -10,10 +10,9 @@ Instagram clone with Express + Prisma + React and React Native
 - [X] Comment on a photo
 - [x] Search by user
 - [x] Search by location
-- [ ] See user profile
-- [ ] Follow / Unfollow User
-- [ ] See the full photo
 - [ ] Edit my profile
+- [ ] See user profile
+- [ ] See the full photo
 - [ ] Upload a photo
 - [ ] Edit the photo (Delete)
 - [ ] See the feed
--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -2,16 +2,22 @@ import { prisma } from "../../../../generated/prisma-client";
 import { generateToken } from "../../../utils";
 
 export default {
-    Mutation: {
-        confirmSecret: async(_, args) => {
-            const {email, secret} = args;
-            const user = await prisma.user({email});
-            if(user.loginSecret === secret){
-                // 사용자 토큰 생성
-                return generateToken(user.id);
-            }else {
-                throw Error("Wrong email/secret combination");
-            }
-        }
-    }
-}
+  Mutation: {
+    confirmSecret: async (_, args) => {
+      const { email, secret } = args;
+      const user = await prisma.user({ email });
+      if (user.loginSecret === secret) {
+        await prisma.updateUser({
+          where: { id: user.id },
+          data: {
+            loginSecret: "",
+          },
+        });
+        // 사용자 토큰 생성
+        return generateToken(user.id);
+      } else {
+        throw Error("Wrong email/secret combination");
+      }
+    },
+  },
+};
