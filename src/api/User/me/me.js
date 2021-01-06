b4b8d4ba5f66358d15a056f908bbe8e94ad6d58b
ssukim <tntntn0930@gmail.com>
2021-01-06 15:58:38 +0900

    delete console log

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -3,7 +3,6 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Query: {
     me: async (_, __, { request, isAuthenticated }) => {
-      // console.log("user parent", _);
       isAuthenticated(request);
       const { user } = request;
       return await prisma.user({id:user.id});
