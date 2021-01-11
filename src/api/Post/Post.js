3fb2438529512b90dc398e9e3a8ef96e061ef36c
ssukim <tntntn0930@gmail.com>
2021-01-11 13:10:18 +0900

    SquarePost Component

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -2,10 +2,10 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
   Post: {
-    files: ({id}) => prisma.post({id}).files(),
-    comments: ({id}) => prisma.post({id}).comments(),
-    user: ({id}) => prisma.post({id}).user(),
-    likes: ({id}) => prisma.post({id}).likes(),
+    files: ({ id }) => prisma.post({ id }).files(),
+    comments: ({ id }) => prisma.post({ id }).comments(),
+    user: ({ id }) => prisma.post({ id }).user(),
+    likes: ({ id }) => prisma.post({ id }).likes(),
     isLiked: (parent, _, { request }) => {
       const { user } = request;
       const { id } = parent;
@@ -27,6 +27,13 @@ export default {
     likeCount: (parent) =>
       prisma
         .likesConnection({
+          where: { post: { id: parent.id } },
+        })
+        .aggregate()
+        .count(),
+    commentCount: (parent) =>
+      prisma
+        .commentsConnection({
           where: { post: { id: parent.id } },
         })
         .aggregate()
